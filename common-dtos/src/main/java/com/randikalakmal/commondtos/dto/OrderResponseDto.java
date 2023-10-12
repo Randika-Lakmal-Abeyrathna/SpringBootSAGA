9dee0d5cc41cc710132d37355f2f08852a745852
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2023-10-12 09:15:56 +0530

    Common Events are created

---
+++ common-dtos/src/main/java/com/randikalakmal/commondtos/dto/OrderResponseDto.java
@@ -0,0 +1,19 @@
+package com.randikalakmal.commondtos.dto;
+
+import com.randikalakmal.commondtos.event.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponseDto {
+
+    private Integer userId;
+    private Integer productId;
+    private Integer amount;
+    private Integer orderId;
+    private OrderStatus orderStatus;
+
+}
---
+++ common-dtos/src/main/java/com/randikalakmal/commondtos/event/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.randikalakmal.commondtos.event;
+
+public enum OrderStatus {
+
+    ORDER_CREATED,ORDER_COMPLETED,ORDER_CANCELLED
+}
